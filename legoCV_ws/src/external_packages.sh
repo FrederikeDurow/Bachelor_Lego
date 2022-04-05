dfdaef0e5feaf89fae3d06dc6fac8afc4de2f888
FrederikeDurow <frdur16@student.sdu.dk>
2022-04-05 10:43:54 +0200

    Update external_packages.sh

--- legoCV_ws/src/external_packages.sh
+++ legoCV_ws/src/external_packages.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/dragandbot/dragandbot_common.git
 git clone https://github.com/ros-perception/image_pipeline.git
+git clone https://github.com/basler/pylon-ros-camera.git
