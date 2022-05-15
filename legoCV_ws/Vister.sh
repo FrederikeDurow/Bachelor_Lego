6c5e08c8b71e71e06e8cf32d21f2818453edfe5b
Rasmus <rasmc19@student.sdu.dk>
2022-05-15 20:46:47 +0200

    Logo moved

---
+++ legoCV_ws/Vister.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/env bash
+source /opt/ros/noetic/setup.bash
+source /home/rasmus/Bachelor/Bachelor_Lego/legoCV_ws/setup.bash
+
+
+gnome-terminal -x sh -c “roscore|less”
+gnome-terminal -x sh -c “roslaunch pylon_camera pylon_camera_node|less”
+gnome-terminal -x sh -c “roslaunch computer_vision computer_vision_start.launch|less”
+
